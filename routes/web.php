ec3cfef2944442128f95e5d794f18ba9e2a1743a
fadwaberrich <fadwa.berrich@esprit.tn>
2023-10-05 23:59:26 +0100

    CRUD category Fadwa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +24,10 @@ Route::get('/front', function () {
 Route::get('/back', function () {
     return view('back/layout');
 });
+Route::get('/category',[CategoryController::class,'show'])->name('showCategory');
+Route::get('/category/form',[CategoryController::class,'form'])->name('formCategory');
+Route::post('/category',[CategoryController::class,'add'])->name('addCategory');
+Route::get('/category/{category}/edit',[CategoryController::class,'edit'])->name('formEditCategory');
+Route::put('/category/{category}/update',[CategoryController::class,'update'])->name('EditCategory');
+Route::delete('/category/{category}/destroy',[CategoryController::class,'destroy'])->name('DeleteCategory');
+Route::get('/category/search',[CategoryController::class,'search'])->name('searchCategory');
