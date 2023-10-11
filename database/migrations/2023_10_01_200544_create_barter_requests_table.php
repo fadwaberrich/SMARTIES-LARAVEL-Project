ecb465ec84f284a7a8f4a072613f07a5ea151289
Nafti Sirine <naftisirin30@gmail.com>
2023-10-11 09:38:53 +0100

    barter request crud

--- database/migrations/2023_10_01_200544_create_barter_requests_table.php
+++ database/migrations/2023_10_01_200544_create_barter_requests_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('barter_requests', function (Blueprint $table) {
             $table->id();
             $table->text('message');
+            $table->text('title');
+            $table->decimal('price', 10, 2); // Add the "price" column with 10 total digits and 2 decimal places
+            $table->string('image')->nullable();
             $table->unsignedBigInteger('barter_id')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->timestamps();
--- resources/views/barterRequests/create.blade.php
+++ resources/views/barterRequests/create.blade.php
@@ -1,18 +1,47 @@
 @extends('front.layout')
 
 @section('content')
+<div class="" style="margin-left:210px;">
     <h1>Create Barter Request</h1>
 
-    <form method="POST" action="{{ route('barterRequests.store') }}">
+    <form method="POST" action="{{ route('barterRequests.store') }}" enctype="multipart/form-data">
         @csrf
-
-        <div class="form-group">
+        <div class="form-group py-2">
+            <label for="title">Title:</label>
+            <input class="form-control" name="title" id="title" rows="4">{{ old('title') }}</input>
+            @error('title')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-group py-2">
             <label for="message">Message:</label>
-            <textarea class="form-control" name="message" id="message" rows="4"></textarea>
+            <textarea class="form-control" name="message" id="message" rows="4">{{ old('message') }}</textarea>
+            @error('message')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <!-- Add Price Field -->
+        <div class="form-group py-2">
+            <label for="price">Price:</label>
+            <input type="number" class="form-control" name="price" id="price" value="{{ old('price') }}">
+            @error('price')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <!-- Add Image Field -->
+        <div class="form-group py-2">
+            <label for="image">Image:</label>
+            <input type="file" class="form-control-file" name="image" id="image">
+            @error('image')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <!-- Add other form fields here, e.g., barter_id, user_id -->
 
         <button type="submit" class="btn btn-primary">Create</button>
     </form>
-    @endsection
+    </div>
+@endsection
