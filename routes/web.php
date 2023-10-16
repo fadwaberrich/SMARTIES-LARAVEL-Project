e0533c8f2083a534f3adac244ff538bbde496a22
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2023-10-17 00:42:44 +0100

    added  complete user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductController;
@@ -18,13 +19,25 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/front', function () {
     return view('front/layout');
 });
 Route::get('/back', function () {
     return view('back/layout');
 });
-
 Route::resource('review-ratings', ReviewController::class);
 Route::get('review-ratings/{id}/edit', 'ReviewController@edit')->name('review-ratings.edit');
 Route::resource('products', ProductController::class);
+
+require __DIR__.'/auth.php';
