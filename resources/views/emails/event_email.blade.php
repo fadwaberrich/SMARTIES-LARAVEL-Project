6a12c14a637bd7a6efd77e4fbd30e9f802be2b53
waelbouwazra <98458757+waelbouwazra@users.noreply.github.com>
2023-10-24 00:29:14 +0200

    added email template

--- resources/views/emails/event_email.blade.php
+++ resources/views/emails/event_email.blade.php
@@ -1,11 +1,206 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Your Event</title>
-</head>
-<body>
-    <h1 style="color: #333; font-size: 24px;">Event Created</h1>
-    <p style="font-size: 16px;">Dear recipient, a new event has been created.</p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content="telephone=no" name="format-detection">
+  <title></title>
+  <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+  <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+  <!--[if gte mso 9]>
+<xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG></o:AllowPNG>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+  <!--[if !mso]><!-- -->
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+  <!--<![endif]-->
+ <!--[if mso]>
+ <style type="text/css">
+     ul {
+  margin: 0 !important;
+  }
+  ol {
+  margin: 0 !important;
+  }
+  li {
+  margin-left: 47px !important;
+  }
 
-</body>
-</html>
+ </style><![endif]
+--></head>
+ <body class="body">
+  <div dir="ltr" class="es-wrapper-color">
+   <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#ceeaf4"></v:fill>
+			</v:background>
+		<![endif]-->
+   <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+    <tbody>
+     <tr>
+      <td class="esd-email-paddings" valign="top">
+       <table cellpadding="0" cellspacing="0" class="esd-header-popover es-header" align="center">
+        <tbody>
+         <tr>
+          <td class="esd-stripe" align="center">
+           <table bgcolor="#ffffff" class="es-header-body" align="center" cellpadding="0" cellspacing="0" width="600">
+            <tbody>
+             <tr>
+              <td class="esd-structure es-p20" align="left">
+               <table cellpadding="0" cellspacing="0" width="100%">
+                <tbody>
+                 <tr>
+                  <td width="560" class="es-m-p0r esd-container-frame" valign="top" align="center">
+                   <table cellpadding="0" cellspacing="0" width="100%">
+                    <tbody>
+                     <tr>
+                      <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank" href="https://viewstripo.email"><img src="https://xwvbdq.stripocdn.email/content/guids/CABINET_79c105c819d9bc5c078b61dd38cef0b6/images/group_116.png" alt="Logo" style="display: block;" height="60" title="Logo"></a></td>
+                     </tr>
+                    </tbody>
+                   </table></td>
+                 </tr>
+                </tbody>
+               </table></td>
+             </tr>
+            </tbody>
+           </table></td>
+         </tr>
+        </tbody>
+       </table>
+       <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+        <tbody>
+         <tr>
+          <td class="esd-stripe" align="center">
+           <table class="es-content-body" style="background-color: #ffffff;" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+            <tbody>
+             <tr>
+              <td class="esd-structure" align="left">
+               <table width="100%" cellspacing="0" cellpadding="0">
+                <tbody>
+                 <tr>
+                  <td class="es-m-p0r es-m-p20b esd-container-frame" width="600" valign="top" align="center">
+                   <table width="100%" cellspacing="0" cellpadding="0">
+                    <tbody>
+                     <tr>
+                      <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank" href="https://viewstripo.email"><img class="adapt-img" src="https://xwvbdq.stripocdn.email/content/guids/CABINET_79c105c819d9bc5c078b61dd38cef0b6/images/4283974_17826converted_h6N.png" alt="" style="display: block;" width="600"></a></td>
+                     </tr>
+                    </tbody>
+                   </table></td>
+                 </tr>
+                </tbody>
+               </table></td>
+             </tr>
+            </tbody>
+           </table></td>
+         </tr>
+        </tbody>
+       </table>
+       <table cellpadding="0" cellspacing="0" class="es-content" align="center">
+        <tbody>
+         <tr>
+          <td class="esd-stripe" align="center">
+           <table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+            <tbody>
+             <tr>
+              <td class="esd-structure es-p30b es-p20r es-p20l" align="left">
+               <table cellpadding="0" cellspacing="0" width="100%">
+                <tbody>
+                 <tr>
+                  <td width="560" class="esd-container-frame" align="center" valign="top">
+                   <table cellpadding="0" cellspacing="0" width="100%">
+                    <tbody>
+                     <tr>
+                      <td align="center" class="esd-block-text es-m-txt-c es-p40r es-p40l es-m-p0r es-m-p0l"><h1>Hey,</h1><h1> A new event is happening soon !</h1><h1> !</h1><p>​</p></td>
+                     </tr>
+                     <tr>
+                      <td align="center" class="esd-block-button es-p20t"><span class="es-button-border"><a href="localhost:8000/eventsfront" class="es-button" target="_blank">Check it out</a></span></td>
+                     </tr>
+                    </tbody>
+                   </table></td>
+                 </tr>
+                </tbody>
+               </table></td>
+             </tr>
+             <tr>
+              <td class="esd-structure" align="left">
+               <table cellpadding="0" cellspacing="0" width="100%">
+                <tbody>
+                 <tr>
+                      
+                  <td width="600" class="esd-container-frame" align="center" valign="top">
+                      <table cellpadding="0" cellspacing="0" width="100%">
+                          <tbody><tr><td align="center" class="esd-empty-container" style="display: none"></td>
+                      </tr></tbody></table>
+                  </td>
+              
+                      
+              </tr>
+                </tbody>
+               </table></td>
+             </tr>
+             
+            </tbody>
+           </table></td>
+         </tr>
+        </tbody>
+       </table>
+       
+       <table cellpadding="0" cellspacing="0" class="es-footer esd-footer-popover" align="center">
+        <tbody>
+         <tr>
+          <td class="esd-stripe" align="center">
+           <table bgcolor="#ffffff" class="es-footer-body" align="center" cellpadding="0" cellspacing="0" width="600">
+            <tbody>
+             <tr>
+              <td class="esd-structure es-p40t es-p20b es-p20r es-p20l" align="left">
+               <table cellpadding="0" cellspacing="0" width="100%">
+                <tbody>
+                 <tr>
+                  <td width="560" class="esd-container-frame" align="center" valign="top">
+                   <table cellpadding="0" cellspacing="0" width="100%">
+                    <tbody>
+                     <tr>
+                      <td align="center" class="esd-block-text es-m-txt-c"><h2>Any questions? <a href="https://viewstripo.email" target="_blank">We're here to help!</a></h2></td>
+                     </tr>
+                     <tr>
+                      <td align="center" class="esd-block-social es-p20t es-p30b" style="font-size:0">
+                       <table cellpadding="0" cellspacing="0" class="es-table-not-adapt es-social">
+                        <tbody>
+                         <tr>
+                          <td align="center" valign="top" class="es-p30r"><a target="_blank" href="https://viewstripo.email"><img title="Facebook" src="https://xwvbdq.stripocdn.email/content/assets/img/social-icons/circle-colored/facebook-circle-colored.png" alt="Fb" height="24"></a></td>
+                          <td align="center" valign="top" class="es-p30r"><a target="_blank" href="https://viewstripo.email"><img title="X.com" src="https://odhvxu.stripocdn.email/content/assets/img/social-icons/circle-colored/x-circle-colored.png" alt="X" height="24"></a></td>
+                          <td align="center" valign="top" class="es-p30r"><a target="_blank" href="https://viewstripo.email"><img title="Instagram" src="https://xwvbdq.stripocdn.email/content/assets/img/social-icons/circle-colored/instagram-circle-colored.png" alt="Inst" height="24"></a></td>
+                          <td align="center" valign="top"><a target="_blank" href="https://viewstripo.email"><img title="Youtube" src="https://xwvbdq.stripocdn.email/content/assets/img/social-icons/circle-colored/youtube-circle-colored.png" alt="Yt" height="24"></a></td>
+                         </tr>
+                        </tbody>
+                       </table></td>
+                     </tr>
+                     <tr>
+                      <td align="center" class="esd-block-text"><p>We don't want you to miss out, but if you have to, you can <a href="https://viewstripo.email" target="_blank">unsubscribe</a>.</p><p><br></p><p>Copyright © 2022 Delivery, All rights reserved.<br><br></p><p>5611 Stony Horse Wharf, Ninilchik, Oregon</p></td>
+                     </tr>
+                    </tbody>
+                   </table></td>
+                 </tr>
+                </tbody>
+               </table></td>
+             </tr>
+             
+            </tbody>
+           </table></td>
+         </tr>
+        </tbody>
+       </table></td>
+     </tr>
+    </tbody>
+   </table>
+  </div>
+ 
+</body></html>
